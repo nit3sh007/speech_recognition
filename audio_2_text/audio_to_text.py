6f918e0749f4b5e186e56b5e4afc391822f9f9e5
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-08-17 02:59:55 -0700

    Add files via upload

---
+++ audio_2_text/audio_to_text.py
@@ -0,0 +1,15 @@
+# using audio file to print audio text as a string
+import speech_recognition as sr
+r = sr.Recognizer()
+harvard = sr.AudioFile('harsh.wav')
+
+#harvard = sr.AudioFile('harvard.wav')
+#harvard = sr.AudioFile('noise.wav')
+
+with harvard as source:
+    #r.adjust_for_ambient_noise(source)
+    # for noise ambient in audio file
+
+    audio = r.record(source)
+print(r.recognize_google(audio))
+
